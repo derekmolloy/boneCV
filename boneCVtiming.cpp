adba6664368771509667182d118302e55de8f742
Derek Molloy <derek@derekmolloy.ie>
2013-05-25 11:39:23 +0100

    OpenCV Source Code Application Code

--- boneCVtiming.cpp
+++ boneCVtiming.cpp
@@ -1,3 +1,14 @@
+/* boneCVtiming.cpp
+ *
+ * Copyright Derek Molloy, School of Electronic Engineering, Dublin City University
+ * www.derekmolloy.ie
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that source code redistributions retain this notice.
+ *
+ * This software is provided AS IS and it comes with no warranties of any type.
+ */
+
 #include<iostream>
 #include<opencv2/opencv.hpp>
 #include<time.h>
